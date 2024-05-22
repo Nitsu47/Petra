9948bb5169fb1ea79656e6a3d8e12a632da108e0
Kiwizacxs <brian.deleon2016@outlook.com>
2024-05-22 12:09:09 -0700

    testing Flask

--- Flask/apps.py
+++ Flask/apps.py
@@ -1,3 +1,17 @@
-#!/bin/bash/python3
-#Necessary imports
+#!/bin/bash/env
 from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<p>Hola</p>"
+
+@app.route("/name/<name>")
+def hello_name(name="Petra"):
+  return f"Hello {escape(name)}"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
