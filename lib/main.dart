1d5f7824bf641ba59fe902a1ecbcf91b6800f498
Teby4 <felipeoliveraamantero@gmail.com>
2024-05-27 21:45:36 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:petratest/screens/home_screen.dart';
 import 'package:petratest/models/bag.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb) {
+    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDpQ5_YanT4UZgpsWRZyEccOXoWyrE2lpM", appId: "1:457519054926:web:ff062545edf31b3a28831f", messagingSenderId: "457519054926", projectId: "petra-25468"))
+  }
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
