3d5c0a482b924dd04420e51ba6f11955737e4189
Nitsu47 <agustraversa2004@gmail.com>
2024-05-31 12:13:57 -0300

    separation of cart item model, cart screen and api to connect flutter and flask, re built of cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,58 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '/screens/cart.dart';
+import '../services/api_service.dart';
+import '../models/cart_item.dart';
 
-class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
+class CartScreen extends StatefulWidget {
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  String _message = '';
+  List<CartItem> _cart = [];
+
+  final ApiService apiService = ApiService();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCart();
+  }
+
+  Future<void> fetchCart() async {
+    try {
+      List<CartItem> cart = await apiService.fetchCart();
+      setState(() {
+        _cart = cart;
+      });
+    } catch (e) {
+      setState(() {
+        _message = 'Failed to load cart';
+      });
+    }
+  }
+
+  Future<void> addToCart(int index) async {
+    try {
+      await apiService.addToCart(index);
+      fetchCart();
+      setState(() {
+        _message = 'Item added to cart';
+      });
+    } catch (e) {
+      setState(() {
+        _message = 'Failed to add item to cart';
+      });
+    }
+  }
+
+  Future<void> deleteFromCart(String nombre, String color, String talle) async {
+    try {
+      await apiService.deleteFromCart(nombre, color, talle);
+      fetchCart();
+      setState(() {
+        _message = 'Item deleted from cart';
+      });
+    } catch (e) {
+      setState(() {
+        _message = 'Failed to delete item from cart';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Carrito de Compras'),
+        title: const Text('Carrito de productos'),
       ),
-      body: Consumer<ShoppingCart>(
-        builder: (context, cart, child) {
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: cart.productNumber,
-                  itemBuilder: (context, index) {
-                    var product = cart.items.values.toList()[index];
-                    return ListTile(
-                      title: Text(product.title),
-                      subtitle: Text('${product.price} x ${product.quantity}'),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.remove),
-                        onPressed: () {
-                          cart.removeProduct(product.id);
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Precio inicial: ${cart.initialPrice.toStringAsFixed(2)}'),
-                    Text('Impuestos: ${cart.taxes.toStringAsFixed(2)}'),
-                    Text('Precio total: ${cart.totalPrice.toStringAsFixed(2)}'),
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  cart.clearCart();
-                },
-                child: const Text('Limpiar Carrito'),
-              ),
-            ],
-          );
-        },
+      body: Column(
+        children: [
+          Text(_message),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _cart.length,
+              itemBuilder: (context, index) {
+                var item = _cart[index];
+                return ListTile(
+                  title: Text(item.nombreProducto),
+                  subtitle: Text('Cantidad: ${item.cantidad}'),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => deleteFromCart(item.nombreProducto, item.color, item.talle),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => addToCart(1),
+        child: const Icon(Icons.add),
       ),
     );
   }
