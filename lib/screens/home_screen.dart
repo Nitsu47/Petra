b11496b319023ce092a589e38763c345d6bb0516
¨Gisezegk¨ <anatanonamewanamedesuka2@gmail.com>
2024-06-03 15:00:03 -0300

    homescreen products positioned

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
