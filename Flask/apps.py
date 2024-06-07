3ab16a99e4c98a975e2c5b93f864c7e8f3dfa796
kiwizacxs <brian.deleon2016@outlook.com>
2024-06-06 18:31:23 -0700

    modified the functions to adap to the new bs

--- Flask/apps.py
+++ Flask/apps.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 from flask import Flask, session, url_for, redirect, request, jsonify, render_template
 from markupsafe import escape
-from flask_cors import CORS
 import json
 
 
 app = Flask(__name__)
 app.secret_key = "Banana"
-CORS(app)
 
 @app.route('/')
 def home():
@@ -34,12 +32,9 @@ def añadir_al_carrito(index):
     product = data[index - 1]
     
     new_product = {
-        "Nombre_Producto": product["Nombre_Producto"],
-        "Color": product.get("Color"),
-        "Talle": product.get("Talle"),
-        "PRRECIO_IVA_INC": product["PRRECIO_IVA_INC"],
+        "name": product["name"],
+        "precio": product["precio"],
         "Cantidad": 1,
-        "Stock": product["Stock"],
     }
     
     if "cart" not in session:
@@ -48,19 +43,17 @@ def añadir_al_carrito(index):
     product_found = False
     for item in session["cart"][1:]:
         if (
-            item["Nombre_Producto"] == new_product["Nombre_Producto"]
-            and item["Color"] == new_product["Color"]
-            and item["Talle"] == new_product["Talle"]
+            item["name"] == new_product["name"]
         ):
             item["Cantidad"] += 1
-            session["cart"][0]["acumulado"] += new_product.get("PRRECIO_IVA_INC")
+            session["cart"][0]["acumulado"] += int(new_product.get("precio").replace('.', ""))
             session["cart"][0]["productos"] += 1
             product_found = True
             break
     
     if not product_found:
         session["cart"].append(new_product)
-        session["cart"][0]["acumulado"] += new_product["PRRECIO_IVA_INC"]
+        session["cart"][0]["acumulado"] += int(new_product["precio"].replace('.', ""))
         session["cart"][0]["productos"] += 1
     
     session.modified = True
@@ -80,21 +73,17 @@ def vaciar_carrito():
 
 @app.route("/eliminar_producto", methods=['GET'])
 def eliminar_producto():
-    Nombre_Producto = request.args.get("Nombre_Producto")
-    Color = request.args.get("Color")
-    Talle = request.args.get("Talle")
+    name = request.args.get("name")
     if "cart" in session:
         for item in session["cart"][1:]:
             if (
-                item["Nombre_Producto"] == Nombre_Producto
-                and item["Color"] == Color
-                and item["Talle"] == Talle
+                item["name"] == name
             ):
                 if item.get("Cantidad") >= 2:
                     item["Cantidad"] -= 1
                 else:
                     session["cart"].remove(item)
-                session["cart"][0]["acumulado"] -= item.get("PRRECIO_IVA_INC")
+                session["cart"][0]["acumulado"] -= int(item.get("precio").replace('.', ""))
                 session["cart"][0]["productos"] -= 1
                 break
     session.modified = True
